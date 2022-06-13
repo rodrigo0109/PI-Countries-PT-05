30d833c040ea2e68728765437bef78c86169c1e3
Rodrigo <rodrigoperez0109@gmail.com>
2022-06-12 22:41:36 -0300

    port

--- api/index.js
+++ api/index.js
@@ -22,8 +22,9 @@ const { conn } = require('./src/db.js');
 const { loadDatabase } = require('./src/loadDatabase')
 
 // Syncing all the models at once.
+const PORT = process.env.PORT || 3001
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(`${PORT}`, () => {
     loadDatabase()
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
